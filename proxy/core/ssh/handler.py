6b52fba900194a26a87d1bcd053d15630175f1b7
sovetnikov <asovetnikov@gmail.com>
2022-03-16 11:52:08 +0300

    Implemented thread for data forwarding between local proxy and SSH remote port tunnel

--- proxy/core/ssh/handler.py
+++ proxy/core/ssh/handler.py
@@ -10,7 +10,12 @@
 """
 import argparse
 from typing import TYPE_CHECKING
+import socket
+import logging
+import select
+import threading
 
+logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:   # pragma: no cover
     from ...common.types import HostPort
@@ -26,10 +31,43 @@ class SshHttpProtocolHandler:
     def __init__(self, flags: argparse.Namespace) -> None:
         self.flags = flags
 
+    def tunnel_forward_worker(self, chan):
+        host, port = '127.0.0.1', self.flags.port
+        sock = socket.socket()
+        try:
+            sock.connect((host, port))
+        except Exception as e:
+            logger.info("Forwarding request to %s:%d failed: %r" % (host, port, e))
+            return
+        logger.info(
+            "Connected!  Tunnel open %r -> %r -> %r"
+            % (chan.origin_addr, chan.getpeername(), (host, port))
+        )
+        while True:
+            r, w, x = select.select([sock, chan], [], [])
+            if sock in r:
+                data = sock.recv(1024)
+                if len(data) == 0:
+                    break
+                chan.send(data)
+            if chan in r:
+                data = chan.recv(1024)
+                if len(data) == 0:
+                    break
+                sock.send(data)
+        chan.close()
+        sock.close()
+        logger.info("Tunnel closed from %r" % (chan.origin_addr,))
+
+
     def on_connection(
-            self,
-            chan: 'Channel',
-            origin: 'HostPort',
-            server: 'HostPort',
+        self,
+        chan: 'Channel',
+        origin: 'HostPort',
+        server: 'HostPort',
     ) -> None:
-        pass
+        thr = threading.Thread(
+            target=self.tunnel_forward_worker, args=(chan,)
+        )
+        thr.setDaemon(True)
+        thr.start()
